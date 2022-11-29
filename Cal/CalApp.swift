7e90de92d9a64e48961351a0d4a7d2d80d4bff64
Omar Dajani <omar_dajani@icloud.com>
2022-11-29 14:38:52 -0700

    Initial Commit

---
+++ Cal/CalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalApp.swift
+//  Cal
+//
+//  Created by Omar Dajani on 11/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
